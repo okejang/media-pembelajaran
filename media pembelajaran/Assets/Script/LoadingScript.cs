e5ba0370ce5dd63251be333567237803c2a6b15a
ade-pc\Ade <athedaisuki@gmail.com>
2016-12-12 15:29:26 +0700

    first project

---
+++ media pembelajaran/Assets/Script/LoadingScript.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class LoadingScript : MonoBehaviour {
+
+	public float loadingTime;
+	public Image loadingBar;
+	public Text percent;
+	public AudioSource musik;
+	public GameObject objekMain;
+	public GameObject objekSetting;
+	public GameObject objekCategory;
+
+	// Use this for initialization
+	void Start () {
+
+		loadingBar.fillAmount = 0;
+		objekMain.SetActive (false);
+		objekSetting.SetActive (false);
+		objekCategory.SetActive (false);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		if (loadingBar.fillAmount <= 1) {
+			loadingBar.fillAmount += 1.0f / loadingTime * Time.deltaTime;
+		}
+		percent.text = "Persiapan " + (loadingBar.fillAmount * 100).ToString ("f0") + " %";
+		if (loadingBar.fillAmount == 1.0f) {
+			//aksi
+			percent.text = "Yuk Belajar . . . ";
+			Destroy(gameObject);
+			musik.Play ();
+			musik.volume = 1f;
+			objekMain.SetActive (true);
+		}
+
+	}
+}
---
+++ media pembelajaran/Assets/Script/MainMenuScript.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour {
+	public GameObject objekSetting;
+	public GameObject objekMenu;
+	public GameObject objekCategory;
+	public AudioSource musik;
+	public Toggle toggleMute;
+	public Slider sliderMusic;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void StartGame(){
+
+		objekCategory.SetActive (true);
+		objekMenu.SetActive (false);
+
+	}
+
+	public void SettingGame(){
+
+		objekSetting.SetActive (true);
+		objekMenu.SetActive (false);
+
+	}
+
+	public void QuitGame(){
+
+		Application.Quit ();
+
+	}
+
+	public void BackMenuGame(){
+
+		objekCategory.SetActive (false);
+		objekSetting.SetActive (false);
+		objekMenu.SetActive (true);
+
+	}
+
+	public void setSound(float vol){
+		
+		if (toggleMute.isOn == false) {
+			musik.volume = vol;
+		}
+
+	}
+
+	public void setMute(){
+
+		if (toggleMute.isOn == true) {
+			musik.volume = 0f;
+		}else {
+			musik.volume = sliderMusic.value;
+		}
+
+	}
+
+}
